1f0228c7cc64558a69bbaf3b74a464c8275095e0
khris22 <khrispunzalan@gmail.com>
2019-12-03 13:49:51 -0800

    updated db schema

--- db/migrate/20191203214510_add_column_to_adventure_table.rb
+++ db/migrate/20191203214510_add_column_to_adventure_table.rb
@@ -1,5 +1,5 @@
 class AddColumnToAdventureTable < ActiveRecord::Migration[6.0]
   def change
-    add_column :adventure, :is_wishlist?, :boolean, default: false
+    add_column :adventures, :is_wishlist?, :boolean, default: false
   end
 end
