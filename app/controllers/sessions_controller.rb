f4191fbd8d1db0e52db1e41c578d5a946b57bba5
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-04 05:47:53 -0800

    created a logout route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,24 @@
 class SessionsController < ApplicationController
     def home
     end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            render 'new'
+        end
+    end
+
+    def destroy
+        session.clear
+        # session.delete(:user_id)
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resources :adventures
   resources :locations
