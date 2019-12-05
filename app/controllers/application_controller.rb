5e5b6e93d228bd96357bd0bd7ea69929f72a5dc7
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-05 15:32:37 -0800

    added helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?, :authenticate_user
+
+    private
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+  
+    def logged_in?
+      !!current_user
+    end
+
+    def authenticate_user
+        if !logged_in?
+          redirect_to root_path
+        end
+    end
+
 end
