3c7eb8aefde68bf97fea4165d1d3a1a91da89e31
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-05 15:33:16 -0800

    created Locations controller & form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,26 @@
 class LocationsController < ApplicationController
+
+    def index
+        @locations = Location.all
+    end
+
+    def new
+        @location = Location.new
+    end
+
+    def create
+        @location = Location.new(location_params)
+        if @location.save
+            redirect_to locations_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def location_params
+        params.require(:location).permit(:city, :state, :description)
+    end
+
 end
