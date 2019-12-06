2bf91f7add6b21cf5a8a19bfb398b56a71c43f05
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-05 18:35:34 -0800

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,18 @@ Rails.application.routes.draw do
 
   get '/auth/github/callback' => 'sessions#create'
 
+  #nested routes
+  resources :users do
+    resources :adventures
+    resources :locations
+  end
+
+
   resources :adventures
   resources :locations
   resources :users
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
