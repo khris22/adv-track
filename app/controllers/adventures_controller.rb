9a6b50d879dc418cd3c36a254faf78cf1c518d33
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-05 18:37:01 -0800

    added create & show on Adventure

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -1,2 +1,16 @@
 class AdventuresController < ApplicationController
+    def create
+        adventure = Adventure.create(user_id: current_user.id, location_id: params[:location][:location_id])
+    end
+
+    def show
+        @adventure = Adventure.find(params[:id])
+    end
+
+    private
+
+    def adventure_params
+        params.require(:adventure).permit(:user_id, :location_id, :name, :recommendation, :is_wishlist?)
+    end
+
 end
