bef13881338f2b09759c9ebe30939e917aa400d8
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-08 20:19:09 -0800

    debugged sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,46 +7,29 @@ class SessionsController < ApplicationController
     end
 
     def create
-        # if auth_hash = request.env["omniauth.auth"]
-        #     oauth_username = request.env["omniauth.auth"]["extra"]["raw_info"]["login"]
-        #     # request.env["omniauth.auth"]["info"]["email"]
-        #     if @user = User.find_by(:username => oauth_username)
-        #         binding.pry
-        #         session[:user_id] = @user.id    
-        #         redirect_to user_path(@user)
-        #     # else
-        #     #     @user = User.new(:email => oauth_email, :password => SecureRandom.hex, :username => oauth_email)
-        #     #     if @user.save
-        #     #         session[:user_id] = @user.id 
-        #     #         redirect_to user_path(@user)
-        #         else
-        #             flash.now[:warning] = "Log In via Github was Unsuccessful. Please sign up"
-        #             render 'new'
-        #         # end
-        #     end     
-        # else
-            @user = User.find_by(username: params[:user][:username])
+        @user = User.find_by(username: params[:user][:username])
             if @user && @user.authenticate(params[:user][:password])
                 session[:user_id] = @user.id 
                 redirect_to adventures_path
-                # redirect_to user_path(@user)
             else
                 flash[:error] = "Sorry, your username or password did not match"
                 render 'new'
-            # end
         end
     end
 
     def ghcreate
         # binding.pry
-        @user = User.find_or_create_by(auth_id: auth['uid']) do |u|
-            # u.auth_id = auth['uid']
-            u.username = auth['info']['name']
-            u.email = auth['info']['email']
-            u.password = auth['uid'] #SecureRandom.hex
+       if @user = User.find_or_create_by(auth_id: auth['uid']) do |u|
+                u.username = auth['info']['name']
+                u.email = auth['info']['email']
+                u.password = SecureRandom.hex #auth['uid']
+            end
+            session[:user_id] = @user.id 
+            redirect_to adventures_path
+        else
+            flash[:error] = "Log In via Github was Unsuccessful. Please sign up"
+            render 'new'
         end
-        session[:user_id] = @user.id 
-        redirect_to adventures_path
     end
 
     def destroy
