b1ace3c6d85a9feed3d80a9e68643858d24817f8
Khris Punzalan <khrispunzalan@gmail.com>
2019-12-12 19:26:35 -0800

    added scope on Location

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,10 +2,7 @@ class Location < ApplicationRecord
     has_many :adventures
     has_many :users, through: :adventures
 
-    # accepts_nested_attributes_for :adventures
-
     validates :city, :state, presence: true
-    # validates :state, uniqueness: true
-    # validates_uniqueness_of  :state, scope: :city
-    # validates_uniqueness_of  :city, scope: :state
+
+    default_scope { order(city: :asc)}
 end
